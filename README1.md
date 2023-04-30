cdda8a239a9753cedbc34701112423e3f089447e
JSR5404 <joshrice5404@gmail.com>
2023-04-30 19:10:55 -0400

    fixed readme generation

---
+++ README1.md
@@ -0,0 +1,27 @@
+# p
+## Description
+p
+![badge](https://img.shields.io/badge/license--brightorange)
+You can access more badges and their purposes at [shields.io](https://shields.io)
+## Table of Contents
+  * [Questions](#questions)
+  * [License](#license)
+    
+    
+      
+## Questions
+      
+  _For further questions:_
+  p
+  
+  _Contact Info:_
+  GitHub: [p](https://github.com/p)
+  Email: [p](mailto:p)
+    
+## License
+      
+  _This application has the ._
+      
+  For more information please view the [license description]().
+  
+  
--- index.js
+++ index.js
@@ -1,197 +1,196 @@
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+
 const fs = require('fs');
-// Array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project? (Required)',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('Please enter your title!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'githubUsername',
-        message: 'What is your GitHub Username? (Required)',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your GitHub username!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address? (Required)',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your email address!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'what',
-        message: 'What is your project and what problem will it solve? (Required)',
-        validate: whatInput => {
-            if (whatInput) {
-                return true;
-            } else {
-                console.log('Please enter what your project is!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'why',
-        message: 'Why did you create this project? (Required)',
-        validate: whyInput => {
-            if (whyInput) {
-                return true;
-            } else {
-                console.log('Please enter why you created this project!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'how',
-        message: 'How will someone use this? (Required)',
-        validate: howInput => {
-            if (howInput) {
-                return true;
-            } else {
-                console.log('Please enter what your project is!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Please provide step-by-step installation instructions for your project. (Required)',
-        validate: installInput => {
-            if (installInput) {
-                return true;
-            } else {
-                console.log('Please enter your installation instructions!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please provide instructions and examples for use. (Required)',
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('Please enter your use instructions!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Which license will you use for your project?',
-        choices: ['agpl', 'apache', 'mit', 'no license']
-    },
-    {
-        type: 'confirm',
-        name: 'confirmContributers',
-        message: 'Would you like to allow other developers to contribute?',
-        default: true
-    },
-    {
-        type: 'input',
-        name: 'contribute',
-        message: 'Please provide guidelines for contributing. (Required)',
-        when: ({ confirmContributers }) => {
-            if (confirmContributers) {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        validate: contributerInput => {
-            if (contributerInput) {
-                return true;
-            } else {
-                console.log('Please enter contributer guidelines!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Please provide instructions on how to test the app. (Required)',
-        validate: testInput => {
-            if (testInput) {
-                return true;
-            } else {
-                console.log('Please enter your use test instructions!');
-                return false;
-            }
-        }
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const util = require('util');
+
+
+// give options for different sections and then add them to 
+// the table of contents in generateMarkdown.js
+const questions = [{
+  type: 'input',
+  name: 'title',
+  message: 'What is the title of your repository? (Required)',
+  //validate to make sure there is a value there
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter your repository title.');
+      return false;
     }
-];
+  }
+},
+{
+  type: 'input',
+  name: 'description',
+  message: 'What is the description of your repository? (Required)',
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter a description of the repository.');
+      return false;
+    }
+  }
+  },
+//confirm whether or not there is a installation process first
+{
+  type: 'confirm',
+  name: 'confirmInstallation',
+  message: 'Is there an installation process?'
+  },
+{
+  type: 'input',
+  name: 'installation',
+  message: 'Please list installation instructions.',
+  // the <when> = if the person selects a installation process allow them to input steps
+  when: ({ confirmInstallation }) => {
+    if (confirmInstallation) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
 
-// function to write README file
-const writeFile = fileContent => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile('./dist/generated-README.md', fileContent, err => {
-            if (err) {
-                reject(err);
-                return;
-            }
+{ //confirm
+  type: 'confirm',
+  name: 'confirmUsage',
+  message: 'Would you like to give instructions for using your application?'
+},
+{ //if confirmed
+  type: 'input',
+  name: 'instructions',
+  message: 'Please list instructions for using your application. It is recommended to add descriptive images later as well.',
+  when: ({ confirmUsage }) => {
+    if (confirmUsage) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
 
-            resolve({
-                ok: true,
-                message: 'File created!'
-            });
-        });
-    });
-};
+{
+  type: 'confirm',
+  name: 'confirmContribution',
+  message: 'May other developers contribute to your repository?'
+},
+{
+  type: 'input',
+  name: 'contribution',
+  message: 'Please explain how other developers may contribute to your project.',
+  when: ({ confirmContribution }) => {
+    if (confirmContribution) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
+{
+  type: 'confirm',
+  name: 'testConfirm',
+  message: 'Is testing available?'
+},
+{
+  type: 'input',
+  name: 'testing',
+  message: 'Please explain how users may test your application.',
+  when: ({ testConfirm }) => {
+    if (testConfirm) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+},
+{ //checkbox that allows license choice
+  type: 'checkbox',
+  name: 'license',
+  message: 'Please choose a license.',
+  choices: ['GNU AGPLv3', 'GNU GPLv3',
+    'GNU LGPLv3', 'Mozilla Public License 2.0',
+    'Apache License 2.0', 'MIT License', 'Boost Software License 1.0',
+    'The Unlicense'],
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please select a license.');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'username',
+  message: 'What is your GitHub username? (Required)',
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter your GitHub username.');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'email',
+  message: 'What is your email address? (Required)',
+  validate: nameInput => {
+    if (nameInput) {
+      return true;
+    } else {
+      console.log('Please enter your email.');
+      return false;
+    }
+  }
+},
+{
+  type: 'input',
+  name: 'questions',
+  message: 'Please list instructions for those who wish to contact you.',
+  validate: (nameInput) => {
+    if (nameInput) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}]; //end of questions array
 
-// function to prompt questions and store user inputs
-const init = () => {
 
-    return inquirer.prompt(questions)
-    .then(readmeData => {
-        return readmeData;
-    })
+// this is an await in the async function
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, error => {
+    if (error) {
+      return console.log('Sorry there was an error : ' + error);
+    }
+  })
 }
 
+
+const createReadMe = util.promisify(writeToFile);
+
+// create async function with catch for errors
+async function init() {
+  try {
+    const userAnswers = await inquirer.prompt(questions);
+    console.log('Thank you! The current data is being processed into your README.md: ', userAnswers);
+    // get markdown template from generateMarkdown.js passing the answers as parameter
+    const myMarkdown = generateMarkdown(userAnswers);
+    console.log(myMarkdown);
+    //write the readme file after the markdown is made
+    await createReadMe('README1.md', myMarkdown);
+    
+  } catch (error) {
+    console.log('Sorry there was an error.' + error);
+  }
+};
+
+
 // Function call to initialize app
-init()
-.then(readmeData => {
-    console.log(readmeData);
-    return generateMarkdown(readmeData);
-})
-.then(pageMD => {
-    return writeFile(pageMD);
-})
-.then(writeFileResponse => {
-    console.log(writeFileResponse.message);
-})
-.catch(err => {
-    console.log(err);
-})
+init();
